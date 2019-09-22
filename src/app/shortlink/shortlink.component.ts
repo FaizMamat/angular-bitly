1c8d25fa4fca119dffce35c59b7d3810cf223d6c
FaizMamat <faiz_mamat@dell.com>
2019-09-23 03:39:53 +0800

    Completed

--- src/app/shortlink/shortlink.component.ts
+++ src/app/shortlink/shortlink.component.ts
@@ -1,37 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
-import { URLService } from '../url.service';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl} from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+
+export interface Data{
+  shortURL: string
+}
 @Component({
   selector: 'app-shortlink',
   templateUrl: './shortlink.component.html',
   styleUrls: ['./shortlink.component.scss']
 })
 export class ShortlinkComponent implements OnInit {
+@Input()
+  urlEnter = new FormControl('');
+  UrlShow = new FormControl('');
 
-  urlInput = new FormControl();
-  urlvalue = 'key in Url';
-  constructor(private URLService: URLService) { }
+  constructor(private http :HttpClient) { 
+ 
+  }
 
-  ngOnInit() {
   
+  ngOnInit() {
+    
   }
 
-
-  onClick() {
-    this.URLService.urlInfo(this.urlInput.value).subscribe(Response => {
-      console.log(this.urlInput);
-      console.log(Response);
-      alert('Submitted Successfully');
-      this.urlInput.reset();
-    });
-   
+  onClick(){
+    let url = "https://tranquil-caverns-37372.herokuapp.com/urls"
+    let params = {
+      "longURL": this.urlEnter.value,
+    }
+  
+    console.log(params)
+    
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      })
+    }
+  
+    this.http.post(url, params, httpOptions ).subscribe((data: Data) => {
+      console.log(data.shortURL)
+       this.UrlShow.setValue(data.shortURL)
+    })
   }
 
+  
 
   onClickReset() {
-    this.urlInput.reset();
+    this.urlEnter.reset();
   }
+ 
 
-
+   
 }
+
+
+
--- src/app/url.service.ts
+++ src/app/url.service.ts
@@ -1,31 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-const postUrl = 'https://tranquil-caverns-37372.herokuapp.com/urls';
-const ListURL = 'https://tranquil-caverns-37372.herokuapp.com/urls';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class URLService {
+  constructor() { }
 
-  constructor(private http: HttpClient) { }
 
-  urlInfo(long) {
-    const params = {
-      longUrl: long
-    };
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-      }),
-    };
-    return this.http.post(`${postUrl}`, params, httpOptions );
 
   }
 
-  shortUrl(id) {
-    return this.http.get(`${ListURL}/${id}`);
-  }
-
-}
